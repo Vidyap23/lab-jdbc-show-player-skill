b62126b481c93a0e625ad8d48e5c8d153cbf8ccf
Vidyap23 <vidyap23@gmail.com>
2020-04-09 13:28:27 +0530

    done

--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,47 @@
+package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Skill;
+import utility.ConnectionManager;
+public class SkillDAO{
+	static List<Skill> skillList = new ArrayList<Skill>();
+	 String insert_skills = "insert into skill values (?,?)";
+	 String select_skills = "select * from skill order by name asc";
+	
+	public void insertSkills(Skill skill) throws SQLException, Exception {
+		PreparedStatement ps = ConnectionManager.getConnection().prepareStatement(insert_skills);
+		ps.setLong(1, skill.getSkillid());
+		ps.setString(2, skill.getSkillname());
+		ps.executeUpdate();
+		
+	}
+	
+	public List<Skill> listAllSkills () throws SQLException, Exception {
+		
+		Skill skill;
+		
+		PreparedStatement ps = ConnectionManager.getConnection().prepareStatement(select_skills);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+	
+		//	System.out.print(rs.getLong("id")+"\t");
+		//	System.out.println(rs.getString("name"));
+			Long id= rs.getLong(1);
+			
+			String name= rs.getString(2);		
+			skill = new Skill(id,name);
+			skill.setSkillid(id);
+			skill.setSkillname(name);
+			skillList.add(skill);
+		}
+		
+		return skillList;
+		
+	}
+}
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,1 +1,40 @@
+package utility;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+
+
+public class ConnectionManager {
+	public static Connection getConnection() throws Exception,SQLException {
+		Properties p = loadPropertiesFile(); //loadproperties returns properties.
+		Class.forName(p.getProperty("driver")); //getting value in jdbc assigned to prop.why seperate file is db may change
+		//create connection object
+		System.out.println(p.getProperty("driver"));
+		System.out.println(p.getProperty("url"));
+		Connection con= null;
+		con= DriverManager.getConnection(p.getProperty("url"),p.getProperty("username"),p.getProperty("password"));
+	
+		return con;
+		
+		
+	}
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();	
+		InputStream in =  ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+	}
+	
+	
+}
+// Can you show me the steps. okay i fork it first. copy the link. go to ubuntu. select directory. git clone and paste the link.
+// Are you cloning the project or dwo. clonigfrom ubuntu. everything else is getting imported. just not that one.cloned twice also sir by deleting it
+	// jdbc properties should be inside src. sir other projects it worked when kept in utility. sir as i said ski
+	
+// In your other project its inside src and not utility. oh okay sir got it. i have one more problem. not able to import day 2 project into eclipse
+	//not getting import do i clone a project and open it sir?
+// I have kept the name as projectt-builder-jd
